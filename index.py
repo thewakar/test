7602ef66295de2280e3d74b7b4601e0ee52a7e3c
thewakar <thewakar1@gmail.com>
2022-09-25 14:49:39 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import MySQLdb
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
